48b72974042930593e8e9697c3219e7f9881f1e8
ChrisLHall <mrchrislhall@gmail.com>
2020-02-01 12:55:52 -0800

    buncha random stuff

---
+++ Assets/Editor/SongInfoEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SongInfoEditor : MonoBehaviour
+{
+    [MenuItem("GGJ/Create song info")]
+    public static void CreateSongInfo()
+    {
+        SongInfo asset = new SongInfo();  //scriptable object
+        asset.bpm = 120f;
+        asset.songStartTime = 0f;
+        AssetDatabase.CreateAsset(asset, "Assets/Songs/info_SONGNAME.asset");
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/Scripts/SongInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongInfo : ScriptableObject
+{
+    public AudioClip song;
+    public float bpm;
+    public float songStartTime;
+    // todo maybe the attached letter?
+}
