a8d6208a08799509cb071c792626a98ba827621a
ChrisLHall <mrchrislhall@gmail.com>
2020-02-01 13:30:07 -0800

    Prefab-izing the scene a bit

---
+++ Assets/Scripts/RhythmGameLogic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmGameLogic : MonoBehaviour
+{
+    public RectTransform beatTrack;
+    public GameObject beatPrefab;
+    public float beatSize = 100;
+    public float distanceBetweenBeats = 200;
+}
