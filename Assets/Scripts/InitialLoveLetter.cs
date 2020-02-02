6c393fc8c5a88cc428ff4f8ec1abcc6eb78662ad
Sixstring982 <sixstring982@gmail.com>
2020-02-01 18:44:31 -0800

    Initial Message Scene
    This change adds a scene which displays the initial message
    to be sent by telegraph.

---
+++ Assets/Scripts/InitialLoveLetter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public sealed class InitialLoveLetter : MonoBehaviour {
+    public TextMeshPro textMesh;
+
+    // Start is called before the first frame update
+    void Start() {
+        textMesh.text = string.Join(" ", TelegramSource.inst.GetLovePoem());
+    }
+}
---
+++ Assets/Scripts/Names.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Names {
+    public const string MAIN_CHARACTER = "Seamus";
+    public const string LOVE_INTEREST = "Trixibell";
+}
