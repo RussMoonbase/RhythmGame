586de072bb98644ee372e9c100da65ac77070716
Sixstring982 <sixstring982@gmail.com>
2020-02-01 17:27:16 -0800

    Use global PRNG in Util.cs
    This change make Util.cs use the global Random, which allows the
    entire game up to this point to be seeded properly.

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Util
-{
-    public static T RandomChoose<T>(T[] array)
-    {
-        return array[Random.Range(0, array.Length)];
+public static class Util {
+    public static T RandomChoose<T>(T[] array) {
+        return array[RandomSource.rand.Next(array.Length)];
     }
 }
