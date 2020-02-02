f41bd460691033b27b67bb97ecdc939f1e8342e8
Ian <russmoonbase@gmail.com>
2020-02-01 19:07:20 -0800

    Added telegram source to the scene

---
+++ Assets/Scripts/CharacterLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLogic : MonoBehaviour
+{
+   float test;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   void IncreaseHappiness()
+   {
+
+   }
+}
