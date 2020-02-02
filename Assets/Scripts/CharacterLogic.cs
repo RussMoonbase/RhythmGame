1d426dd71e4487807e8eb60582fc1e8b847f6cb1
Ian <russmoonbase@gmail.com>
2020-02-01 19:29:16 -0800

    Added character logic script and tested dancing

--- Assets/Scripts/CharacterLogic.cs
+++ Assets/Scripts/CharacterLogic.cs
@@ -4,22 +4,52 @@ using UnityEngine;
 
 public class CharacterLogic : MonoBehaviour
 {
-   float test;
+   float _happinessFactor = 0;
+   public KeyCode increaseKey;
+   public KeyCode decreaseKey;
+
+   Animator _animator;
+
 
    // Start is called before the first frame update
    void Start()
    {
-
+      _animator = this.gameObject.GetComponent<Animator>();
    }
 
    // Update is called once per frame
    void Update()
    {
+      if (Input.GetKeyDown(increaseKey))
+      {
+         IncreaseHappiness();
+      }
 
+      if (Input.GetKeyDown(decreaseKey))
+      {
+         DecreaseHappiness();
+      }
    }
 
    void IncreaseHappiness()
    {
+      _happinessFactor += 0.1f;
+      Debug.Log("HappinessFactor = " + _happinessFactor);
 
+      if (_animator)
+      {
+         Debug.Log("there is an animator");
+         _animator.SetFloat("HappinessFactor", _happinessFactor);
+      }
+   }
+
+   void DecreaseHappiness()
+   {
+      _happinessFactor -= 0.1f;
+
+      if (_animator)
+      {
+         _animator.SetFloat("HappinessFactor", _happinessFactor);
+      }
    }
 }
