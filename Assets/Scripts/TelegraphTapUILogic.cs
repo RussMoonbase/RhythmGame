2d7a1d73403ab7a67caf6e515abf13412b2b1a4e
Ian <russmoonbase@gmail.com>
2020-02-02 15:26:10 -0800

    Added tap telegraph ui

---
+++ Assets/Scripts/TelegraphTapUILogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelegraphTapUILogic : MonoBehaviour
+{
+   Image _image;
+   [SerializeField] KeyCode _tapKey;
+   [SerializeField] Sprite _offSprite;
+   [SerializeField] Sprite _onSprite;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _image = GetComponent<Image>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetKeyDown(_tapKey))
+      {
+         _image.sprite = _onSprite;
+      }
+
+      if (Input.GetKeyUp(_tapKey))
+      {
+         _image.sprite = _offSprite;
+      }
+   }
+}
