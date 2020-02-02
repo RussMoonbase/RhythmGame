91db79d39880c987aadab3122d99a22eb3cbff33
Ian <russmoonbase@gmail.com>
2020-02-02 10:31:42 -0800

    Added placeholder hand to scene

---
+++ Assets/Scripts/HandLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLogic : MonoBehaviour
+{
+   Animator _animator;
+   [SerializeField] KeyCode _tapKey;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _animator = GetComponentInChildren<Animator>();
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetKeyDown(_tapKey))
+      {
+         TapDown();
+      }
+   }
+
+   public void TapDown()
+   {
+      if (_animator)
+      {
+         _animator.SetTrigger("Tapped");
+      }
+   }
+
+   public void HoldDown()
+   {
+
+   }
+}
