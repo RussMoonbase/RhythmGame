e0de65b8e3f0fff8b201ef58cd53974f5455a59b
ChrisLHall <mrchrislhall@gmail.com>
2020-02-02 12:12:45 -0800

    Stubbed in hype-measurer

---
+++ Assets/Scripts/MeasureHype.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeasureHype : MonoBehaviour
+{
+    public float Hype { get; private set; } = 0f;
+
+    void Start()
+    {
+        RhythmGameLogic.inst.OnBeatPressed += Inst_OnBeatPressed;
+        RhythmGameLogic.inst.OnBeatScored += Inst_OnBeatScored;
+        RhythmGameLogic.inst.OnMissed += Inst_OnMissed;
+    }
+
+    void OnDestroy()
+    {
+        if (null != RhythmGameLogic.inst)
+        {
+            RhythmGameLogic.inst.OnBeatPressed -= Inst_OnBeatPressed;
+            RhythmGameLogic.inst.OnBeatScored -= Inst_OnBeatScored;
+            RhythmGameLogic.inst.OnMissed -= Inst_OnMissed;
+        }
+    }
+
+    private void Inst_OnBeatPressed(BeatBlock block)
+    {
+        
+    }
+
+    private void Inst_OnBeatScored(BeatBlock block, float score)
+    {
+        if (score == 2f)
+        {
+            Hype += .05f;
+        }
+        else if (score > 2f)
+        {
+            Hype += .1f;
+        }
+        else
+        {
+            Hype -= .1f;
+        }
+
+        Hype = Mathf.Clamp01(Hype);
+    }
+
+    private void Inst_OnMissed(BeatBlock block)
+    {
+        Hype -= .1f;
+        Hype = Mathf.Clamp01(Hype);
+    }
+
+}
